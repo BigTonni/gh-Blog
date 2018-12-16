916fca5e3c117f30493ad347c4bc2e13414da11f
Konstantin Horodnuk <horodnuk@gmail.com>
2018-12-16 17:58:22 +0200

    added KnpMenuBundle

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'posts_show']);
+        $menu->addChild('Create Post', ['route' => 'post_create']);
+
+
+        return $menu;
+    }
+}
